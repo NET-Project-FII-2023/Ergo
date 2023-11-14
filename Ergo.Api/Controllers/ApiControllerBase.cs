2ba3385bb651a198115639cfd9d6e63799aa04ab
GeorgeDenisDuluta <george.duluta@yahoo.com>
2023-11-14 15:00:25 +0200

    Added ApiControllerBase

---
+++ Ergo.Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ergo.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ApiControllerBase : ControllerBase
+    {
+        private ISender mediator = null!;
+
+        protected ISender Mediator => mediator
+        ??= HttpContext.RequestServices
+            .GetRequiredService<ISender>();
+    }
+}
