b1c6c8bdd9d9b65d0aae332a9faf8b15b26d5a2c
GeorgeDenis <george.duluta@yahoo.com>
2023-12-12 19:51:47 +0200

    update profile started

--- Ergo.App/Contracts/IUserDataService.cs
+++ Ergo.App/Contracts/IUserDataService.cs
@@ -1,10 +1,12 @@
-﻿using Ergo.App.ViewModels;
+﻿using Ergo.App.Services.Responses;
+using Ergo.App.ViewModels;
 
 namespace Ergo.App.Contracts
 {
     public interface IUserDataService
     {
         Task<UserViewModel> GetUserByEmailAsync(string email);
+        Task<ApiResponse<UpdateUserDto>> UpdateUserAsync(string id, UpdateUserDto updateUserDto);
 
     }
 }
