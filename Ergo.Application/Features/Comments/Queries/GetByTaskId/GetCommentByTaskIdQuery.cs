bd9565c7ee82224a4963148b1ac5cfb1483399fc
Galatianu Mihai <mgalatianu@yahoo.ro>
2023-12-12 18:32:12 +0200

    GetCommentsByTaskId

---
+++ Ergo.Application/Features/Comments/Queries/GetByTaskId/GetCommentByTaskIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Ergo.Application.Features.Comments.Queries.GetByTaskId;
+
+namespace Ergo.Application.Features.Comments.Queries.GetByTaskId
+{
+	public class GetCommentByTaskIdQuery : IRequest<GetCommentByTaskIdQueryResponse>
+	{
+		public Guid TaskId { get; set; }
+	}
+}
---
+++ Ergo.Application/Features/Comments/Queries/GetByTaskId/GetCommentByTaskIdQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿using Ergo.Application.Responses;
+
+namespace Ergo.Application.Features.Comments.Queries.GetByTaskId
+{
+	public class GetCommentByTaskIdQueryResponse : BaseResponse
+	{
+		public List<CommentDto> Comments { get; set; }
+	}
+}
