39ac4083f6d433f82123642191483852710edb0b
tudstk <tudorstroescu@yahoo.com>
2023-12-20 10:49:20 +0200

    fixed update task modal

--- Ergo.App/ViewModels/UpdateTaskViewModel.cs
+++ Ergo.App/ViewModels/UpdateTaskViewModel.cs
@@ -1,7 +1,27 @@
+using Ergo.Domain.Entities.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace Ergo.App.ViewModels;
 
-public class UpdateTaskViewModel : TaskViewModel
+public class UpdateTaskViewModel
 {
+    public Guid TaskItemId { get; set; }
+    [Required(ErrorMessage = "Task name is required.")]
+    [StringLength(100, ErrorMessage = "Task name must be 100 characters or less.")]
+    public string? TaskName { get; set; }
+
+    [StringLength(500, ErrorMessage = "Description must be 500 characters or less.")]
+    public string? Description { get; set; }
+
+    [Required(ErrorMessage = "Deadline is required.")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    public DateTime Deadline { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    [Required(ErrorMessage = "Project ID is required.")]
+    public Guid ProjectId { get; set; }
+
+    public TaskState? State { get; set; } = TaskState.ToDo;
 }
