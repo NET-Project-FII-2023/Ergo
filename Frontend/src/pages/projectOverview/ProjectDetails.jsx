574c294de3b5afeadbd5a54a24eeb0fd96d37596
Tudor Stroescu <tudorstroescu@yahoo.com>
2024-01-31 17:56:39 +0200

    Display fetched details of the current project

--- Frontend/src/pages/projectOverview/ProjectDetails.jsx
+++ Frontend/src/pages/projectOverview/ProjectDetails.jsx
@@ -7,6 +7,7 @@ import { Card, CardContent, Typography, Modal, Backdrop, Fade } from '@mui/mater
 const ProjectDetails = () => {
   const { projectId } = useParams();
   const [taskItems, setTaskItems] = useState([]);
+  const [currentProject, setCurrentProject] = useState([]);
   const [selectedTask, setSelectedTask] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
   const { token, userId } = useUser();
@@ -16,6 +17,7 @@ const ProjectDetails = () => {
 
   useEffect(() => {
     fetchTaskItems();
+    fetchCurrentProject();
   }, [projectId, token, userId]);
 
   const fetchTaskItems = async () => {
@@ -40,12 +42,38 @@ const ProjectDetails = () => {
     }
   };
 
+  const fetchCurrentProject = async () => {
+    try {
+      if (!token || !userId) return;
+
+      const response = await api.get(`/api/v1/Projects/${projectId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+        setCurrentProject(response.data)
+        console.log("Project:");
+        console.log(response.data);
+      } else {
+        console.error('Error fetching tasks:', response);
+      }
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  };
+
   const renderTaskCards = (tasks) => {
     return tasks.map(taskItem => (
-      <Card key={taskItem.taskItemId} className={`mb-4 opacity-80 cursor-pointer`} 
-      style={{ backgroundColor: getCardBackgroundColor(taskItem.state), }}
-      onClick={() => handleOpenModal(taskItem)}       
-      >
+      <Card
+      key={taskItem.taskItemId}
+      className={`mb-4 opacity-80 cursor-pointer`}
+      style={{
+        backgroundColor: getCardBackgroundColor(taskItem.state),
+      }}
+      onClick={() => handleOpenModal(taskItem)}
+    >
         <CardContent>
           <Typography variant="h6" gutterBottom>
             {taskItem.taskName}
@@ -64,11 +92,11 @@ const ProjectDetails = () => {
   const getCardBackgroundColor = (state) => {
     switch (state) {
       case 1:
-        return 'purple-500';
+        return 'rgba(205, 122, 235, 0.7)'; // Purple
       case 2:
-        return 'cyan-300';
+        return 'rgba(111, 206, 237, 0.7)'; // Cyan
       case 3:
-        return 'lime-500';
+        return 'rgba(144, 238, 144, 0.7)'; // Lime
       default:
         return 'white';
     }
@@ -86,6 +114,22 @@ const ProjectDetails = () => {
   return (
     <div>
       <div>
+        <Typography variant="h3">
+          {currentProject.projectName}
+        </Typography>
+        <div className='flex flex-row'>
+          <Typography variant="body1" color="textPrimary" component="p" mr={1}>
+            Description:   
+          </Typography>
+          <Typography variant="body1" color="textSecondary" component="p">
+            {currentProject.description}
+          </Typography>
+        </div>
+      </div>
+      
+      <div className="border-b-2 border-gray-300 my-4"></div>
+  
+      <div>
         <div className="flex justify-between">
           <div className="flex-1 mr-8">
             <h4 className="mb-2">To Do</h4>
@@ -101,18 +145,13 @@ const ProjectDetails = () => {
           </div>
         </div>
       </div>
-
+  
       <Modal
         open={modalOpen}
         onClose={handleCloseModal}
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
         closeAfterTransition
-        BackdropProps={{
-          style: {
-            outline: 'none',
-          },
-        }}
       >
         <Fade in={modalOpen}>
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 max-h-80 overflow-y-auto bg-white rounded-md shadow-md p-8">
@@ -135,6 +174,7 @@ const ProjectDetails = () => {
       </Modal>
     </div>
   );
+  
 };
 
 export default ProjectDetails;
