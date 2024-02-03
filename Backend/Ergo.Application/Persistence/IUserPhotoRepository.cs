b345b01bd65f5d16e7388cd5e4f4f95315693638
GeorgeDenis <george.duluta@yahoo.com>
2024-02-03 11:32:28 +0200

    user profile photo upload/update

--- Backend/Ergo.Application/Persistence/IUserPhotoRepository.cs
+++ Backend/Ergo.Application/Persistence/IUserPhotoRepository.cs
@@ -1,9 +1,12 @@
-﻿using Ergo.Domain.Entities;
+﻿using Ergo.Domain.Common;
+using Ergo.Domain.Entities;
 
 namespace Ergo.Application.Persistence
 {
     public interface IUserPhotoRepository : IAsyncRepository<UserPhoto>
     {
+        //method to get a user photo by user id
+        Task<Result<string>> GetUserPhotoByUserId(string userId);
 
     }
 }
