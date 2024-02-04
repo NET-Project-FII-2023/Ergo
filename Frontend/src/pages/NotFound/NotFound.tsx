4671e714e3307c6b6c73dcea79e8e445d6203d2b
GeorgeDenis <george.duluta@yahoo.com>
2024-02-05 01:33:00 +0200

    Not Found Page

--- Frontend/src/pages/NotFound/NotFound.tsx
+++ Frontend/src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
+    <div className="bg-surface-dark">
+      <div className="grid h-screen place-content-center px-4">
+        <div className="text-center">
+          <h1 className="text-9xl font-black text-gray-400">404</h1>
+          <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
+          <p className="mt-4 text-surface-light">We can't find that page.</p>
+          <Link to="dashboard/home" className="mt-6 inline-block rounded bg-secondary px-5 py-3 text-sm font-medium text-white hover:bg-primary focus:outline-none focus:ring">
+      Go Back Home
+    </Link>
+        </div>
+      </div>
     </div>
   );
 }
