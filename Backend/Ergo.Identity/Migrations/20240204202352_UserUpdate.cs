05bef3dabae22f3e4182cb62ea019a85b7db147c
GeorgeDenis <george.duluta@yahoo.com>
2024-02-04 22:24:49 +0200

    UserDatabaseUpdate

---
+++ Backend/Ergo.Identity/Migrations/20240204202352_UserUpdate.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ergo.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Company",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Facebook",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GitHub",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instagram",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedIn",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mobile",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TwitterX",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Company",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Facebook",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GitHub",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Instagram",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LinkedIn",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Mobile",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TwitterX",
+                table: "AspNetUsers");
+        }
+    }
+}
