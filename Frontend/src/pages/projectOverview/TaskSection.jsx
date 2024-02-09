af010d201768860eee64621b61777df6511f7b77
Tudor Stroescu <tudorstroescu@yahoo.com>
2024-02-09 03:34:34 +0200

    Partially working drag and drop

--- Frontend/src/pages/projectOverview/TaskSection.jsx
+++ Frontend/src/pages/projectOverview/TaskSection.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import api from "@/services/api";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import TaskCard from './TaskCard';
 import AddTask from './AddTask';
 import MembersList from './MembersList';
@@ -9,107 +10,190 @@ const TaskSection = ({ projectId, token, userId, handleOpenModal}) => {
     const todoTasks = taskItems.filter(taskItem => taskItem.state === 1);
     const inProgressTasks = taskItems.filter(taskItem => taskItem.state === 2);
     const doneTasks = taskItems.filter(taskItem => taskItem.state === 3);
-  
+
     useEffect(() => {
-      fetchTaskItems();
+        fetchTaskItems();
     }, [projectId, token, userId]);
 
-  useEffect(() => {
-    fetchTaskItems();
-  }, [projectId, token, userId]);
+    const fetchTaskItems = async () => {
+        try {
+            if (!token || !userId) return;
 
-  const fetchTaskItems = async () => {
-    try {
-      if (!token || !userId) return;
+            const response = await api.get(`/api/v1/TaskItems/ByProject/${projectId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
 
-      const response = await api.get(`/api/v1/TaskItems/ByProject/${projectId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+            if (response.status === 200) {
+                setTaskItems(response.data.taskItems);
+                console.log("Tasks:");
+                console.log(response.data.taskItems);
+            } else {
+                console.error('Error fetching tasks:', response);
+            }
+        } catch (error) {
+            console.error('Error fetching tasks:', error);
+        }
+    };
 
-      if (response.status === 200) {
-        setTaskItems(response.data.taskItems);
-        console.log("Tasks:");
-        console.log(response.data.taskItems);
-      } else {
-        console.error('Error fetching tasks:', response);
-      }
-    } catch (error) {
-      console.error('Error fetching tasks:', error);
-    }
-  };
-
-  const renderTaskCards = (tasks, column) => {
-    let color;
-    switch (column) {
-      case 'todo':
-        color = "#5e4d8c";
-        break;
-      case 'inProgress':
-        color = "#3f6da6";
-        break;
-      case 'done':
-        color = "#42a696"; 
-        break;
-      default:
-        color = "#2dd4bf";
-    }
+    const handleTaskItemDrop = async (result) => {
+      const { source, destination, draggableId } = result;
   
-    return tasks.map((taskItem) => (
-      <TaskCard
-        key={taskItem.taskItemId}
-        task={taskItem}
-        handleOpenModal={() => handleOpenModal(taskItem)}
-        color={color}
-      />
-    ));
+      if (!destination) {
+          return;
+      }
+  
+      if (source.droppableId === destination.droppableId && source.index === destination.index) {
+          return;
+      }
+  
+      const updatedTaskItems = Array.from(taskItems);
+      const movedTaskItemIndex = updatedTaskItems.findIndex(taskItem => taskItem.taskItemId === draggableId);
+      const movedTaskItem = updatedTaskItems[movedTaskItemIndex];
+      const newColumnState = getColumnState(destination.droppableId);
+      movedTaskItem.state = newColumnState;
+      setTaskItems(updatedTaskItems);
+  
+      try {
+          console.log('Request body:', movedTaskItem); // Log the request body
+          const response = await api.put(`/api/v1/TaskItems/${movedTaskItem.taskItemId}`, {
+              ...movedTaskItem
+          }, {
+              headers: {
+                  Authorization: `Bearer ${token}`,
+              },
+          });
+  
+          if (response.status !== 200) {
+              console.error('Error updating task item:', response);
+          }
+      } catch (error) {
+          console.error('Error updating task item:', error);
+      }
   };
+  
+  const getColumnState = (droppableId) => {
+    switch (droppableId) {
+        case 'todo': // String value matching with "1"
+            return 1;
+        case 'inProgress': // String value matching with "2"
+            return 2;
+        case 'done': // String value matching with "3"
+            return 3;
+        default:
+            return null;
+    }
+};
+  
 
-
-  return (
-    <div className='w-[75vw]'>
-      <div className="border-b-2 border-surface-dark my-4"></div>
-      <div>
-        <div className="flex justify-evenly">
-          <div className="w-[25%] mr-4">
-            <div className="border-b-4 border-secondary"></div>
-            <div className='p-3 bg-surface-dark mb-4 flex items-center rounded-b'>
-              <h4 className="text-surface-light">To do</h4>
+    return (
+        <DragDropContext onDragEnd={handleTaskItemDrop}>
+            <div className='w-[75vw]'>
+                <div className="border-b-2 border-surface-dark my-4"></div>
+                <div>
+                    <div className="flex justify-evenly">
+                        <Droppable droppableId='todo'>
+                            {(provided) => (
+                                <div ref={provided.innerRef} {...provided.droppableProps} className="w-[25%] mr-4">
+                                    <div className="border-b-4 border-secondary"></div>
+                                    <div className='p-3 bg-surface-dark mb-4 flex items-center rounded-b'>
+                                        <h4 className="text-surface-light">To do</h4>
+                                    </div>
+                                    <AddTask
+                                        projectId={projectId}
+                                        token={token}
+                                        userId={userId}
+                                        onTaskAdded={fetchTaskItems}
+                                    />
+                                    {todoTasks.map((taskItem, index) => (
+                                        <Draggable key={taskItem.taskItemId} draggableId={taskItem.taskItemId} index={index}>
+                                            {(provided) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                    <TaskCard
+                                                        task={taskItem}
+                                                        handleOpenModal={() => handleOpenModal(taskItem)}
+                                                        color="#5e4d8c"
+                                                    />
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                        <Droppable droppableId='inProgress'>
+                            {(provided) => (
+                                <div ref={provided.innerRef} {...provided.droppableProps} className="w-[25%] mr-4">
+                                    <div className="border-b-4 border-blue-400"></div>
+                                    <div className='p-3 bg-surface-dark mb-4 flex items-center rounded-b'>
+                                        <h4 className="text-surface-light">In progress</h4>
+                                    </div>
+                                    {inProgressTasks.map((taskItem, index) => (
+                                        <Draggable key={taskItem.taskItemId} draggableId={taskItem.taskItemId} index={index}>
+                                            {(provided) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                    <TaskCard
+                                                        task={taskItem}
+                                                        handleOpenModal={() => handleOpenModal(taskItem)}
+                                                        color="#3f6da6"
+                                                    />
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                        <Droppable droppableId='done'>
+                            {(provided) => (
+                                <div ref={provided.innerRef} {...provided.droppableProps} className="w-[25%] text-surface-light">
+                                    <div className="border-b-4 border-teal-300"></div>
+                                    <div className='p-3 bg-surface-dark mb-4 flex items-center rounded-b'>
+                                        <h4 className="text-surface-light">Done</h4>
+                                    </div>
+                                    {doneTasks.map((taskItem, index) => (
+                                        <Draggable key={taskItem.taskItemId} draggableId={taskItem.taskItemId} index={index}>
+                                            {(provided) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                    <TaskCard
+                                                        task={taskItem}
+                                                        handleOpenModal={() => handleOpenModal(taskItem)}
+                                                        color="#42a696"
+                                                    />
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                        <div className="w-[25%] text-surface-light bg-surface-darkest px-4 ml-3">
+                            <MembersList
+                                projectId={projectId}
+                                token={token}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <AddTask
-              projectId={projectId}
-              token={token}
-              userId={userId}
-              onTaskAdded={fetchTaskItems}
-            />
-            {renderTaskCards(todoTasks, 'todo')}
-          </div>
-          <div className="w-[25%] mr-4">
-            <div className="border-b-4 border-blue-400"></div>
-            <div className='p-3 bg-surface-dark mb-4 flex items-center rounded-b'>
-              <h4 className="text-surface-light">In progress</h4>
-            </div>
-            {renderTaskCards(inProgressTasks, 'inProgress')}
-          </div>
-          <div className="w-[25%] text-surface-light">
-            <div className="border-b-4 border-teal-300"></div>
-            <div className='p-3 bg-surface-dark mb-4 flex items-center rounded-b'>
-              <h4 className="text-surface-light">Done</h4>
-            </div>
-            {renderTaskCards(doneTasks, 'done')}
-          </div>
-          <div className="w-[25%] text-surface-light bg-surface-darkest px-4 ml-3">
-
-            <MembersList
-              projectId={projectId}
-              token={token}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+        </DragDropContext>
+    );
 };
 
 export default TaskSection;
