94590ad1b2493f17849d30462f3a1c3465449d31
lGnyte <grasu.ioan02@gmail.com>
2024-02-13 23:30:19 +0200

    fixed Backend borș

--- Backend/Ergo.Application/Persistence/IAsyncRepository.cs
+++ Backend/Ergo.Application/Persistence/IAsyncRepository.cs
@@ -10,5 +10,4 @@ public interface IAsyncRepository<T> where T : class
     Task<Result<T>> DeleteAsync(Guid id);
     Task<Result<IReadOnlyList<T>>> GetPagedResponseAsync(int page, int size);
     Task<Result<IReadOnlyList<T>>> GetAllAsync();
-    Task<Result<IReadOnlyList<T>>> GetCommentByTaskIdAsync(Guid taskId);
 }
